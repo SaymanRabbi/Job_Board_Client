d8da2c8e7b980f85e78f208659877ef4ce4d1f8a
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-09 21:43:46 +0600

    firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyATiCXL85UxFathe0HmhOpegzP7rTI9los",
-  authDomain: "job-box-15b9e.firebaseapp.com",
-  projectId: "job-box-15b9e",
-  storageBucket: "job-box-15b9e.appspot.com",
-  messagingSenderId: "542707116484",
-  appId: "1:542707116484:web:a5430e1806bc4f0fec5c91"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
