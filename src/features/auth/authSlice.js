807a002eac2aa98e2f74eabab9dcb50ec0db3bef
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-09 21:03:24 +0600

    create auth slice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,17 @@
+
+const initialState = {
+    email:"",
+    role:"",
+    isLoading:true,
+    isError:false,
+    errorMessage:""
+};
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{}
+})
+
+
+export default authSlice.reducer;
